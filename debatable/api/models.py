4c7c5754b8d94700b7caef5558184fa049766a10
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-04-03 15:34:08 -0400

    model for post added

--- debatable/api/models.py
+++ debatable/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import string
+import random
 
-# Create your models here.
+def generateUrlId():
+    choices = string.ascii_lowercase + string.ascii_lowercase + string.digits + '-_'
+    while True:
+        res = ''.join(random.choices(choices, k=12))
+        if Post.objects.filter(url_id=res).count() == 0:
+            return res
+
+class Post(models.Model):
+    url_id = models.CharField(max_length=12, default=generateUrlId)
+    poster = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=60, default='')
+    body = models.CharField(max_length=10000, default='')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
