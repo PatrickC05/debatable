b79056e63d2084a0e0be53d8f2707c58113a7ab4
Alex <66565042+alexshieh25@users.noreply.github.com>
2021-04-04 08:12:41 -0400

    updated posts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React, {useState} from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useParams } from 'react-router-dom';
 import Home from './components/Home';
 import Create from './components/Create';
 import Login from './components/Login';
 import Register from './components/Register';
+import Post from './components/Post';
 import { UserContext } from "./components/UserContext";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
                     <Route exact path="/create" component={Create} />
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/register" component={Register} />
+                    <Route exact path="/post/:id" component={Post} />
                 </UserContext.Provider>
             </Switch>
         </main>
--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { useParams } from 'react-router-dom';
 
 export default function Post() {
+  let {id} = useParams();
   return (
-    <h1>{id}</h1>
-    </br>
-    <p></p>
+    <div>
+      <h1>{id}</h1>
+      <br></br>
+      <p></p>
+    </div>
   )
 }
