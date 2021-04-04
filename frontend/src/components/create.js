856ed331ac1671ae6f54fae187d74ac1241123da
Alex <66565042+alexshieh25@users.noreply.github.com>
2021-04-03 22:23:35 -0400

    updated use state

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 
 export default function Create() {
   const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
   return (
     <form>
       <input type="text" min="5" max="60" placeholder="Enter your title" onChange={(e)=>setTitle(e.target.value)} value={title}/>
-      <textarea min="300" max="10000" placeholder="What is your opinion?"></textarea>
+      <textarea min="300" max="10000" placeholder="What is your opinion?"onChange={(e)=>setContent(e.target.value)} value={content}></textarea>
       <input type="submit" value="Submit"/>
     </form>
   )
