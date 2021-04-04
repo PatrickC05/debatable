59e08e69d01de37bf7329176634815b6a191ac29
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-04-04 11:04:12 -0400

    voting works

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -1,11 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { UserContext } from "./UserContext";
 import { FiThumbsUp, FiThumbsDown } from 'react-icons/fi'
 import { useParams } from 'react-router-dom';
+
 
 export default function Post() {
   let {id} = useParams();
   const [content, setContent] = useState('');
   const [vote, setVote] = useState({"Agree":0, "Disagree":0})
+  const [myVote, setMyVote] = useState(0)
+  const {user, setUser} = useContext(UserContext);
 
   useEffect(()=>{
     fetch('http://127.0.0.1:8000/api/post/'+id, {
@@ -22,13 +26,36 @@ export default function Post() {
     .then(res=>setVote(res))
   }, [])
 
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/vote/'+id, {
+      "method": "PUT",
+      "headers": {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token '+user['token']
+      },
+      "body": JSON.stringify({'vote': myVote})
+    })
+    .then(res=>res.json())
+    .then(res=>setVote(res))
+  }, [myVote])
+
+  function handleClick(e) {
+    let txt = e.target.textContent
+    if (txt == 'Agree') {
+      setMyVote(1)
+    }
+    else {
+      setMyVote(-1)
+    }
+  }
+
   return (
     <div className='post'>
       <h1 className='title'>{content["title"]}</h1>
       <br></br>
       <p>{content["body"]}</p>
-      <p>{vote["Agree"]}<FiThumbsUp /></p>
-      <p>{vote["Disagree"]} <FiThumbsDown /></p>
+      <p><button onClick={handleClick}>Agree</button>{vote["Agree"]}</p>
+      <p><button onClick={handleClick}>Disagree</button>{vote["Disagree"]}</p>
     </div>
   )
 }
