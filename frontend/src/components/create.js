ae437a4b1ca24c29ff44bcb564faa61606330c9c
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-04-03 22:21:54 -0400

    usestate added

--- frontend/src/components/create.js
+++ frontend/src/components/create.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
+  const [title, setTitle] = useState('')
   return (
     <form>
-      <input type="text" min="5" max="60" placeholder="Enter your title"/>
+      <input type="text" min="5" max="60" placeholder="Enter your title" onChange={(e)=>setTitle(e.target.value)} value={title}/>
       <textarea min="300" max="10000" placeholder="What is your opinion?"></textarea>
       <input type="submit" value="Submit"/>
     </form>
